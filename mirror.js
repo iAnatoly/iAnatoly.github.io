74d1119ce2083ebed51a2ca08ab446757c915928
iAnatoly <anatoly.ivanov@gmail.com>
2018-01-26 11:28:00 -0800

    moving everything under document-ready

--- mirror.js
+++ mirror.js
@@ -1,45 +1,46 @@
-navigator
-    .mediaDevices
-    .getUserMedia({ video: true, audio: false },)
-    .then(function(stream){
-        var mirror = document.querySelector('#video');
-        mirror.srcObject = stream;
-        mirror.play();        
-    })
-    .catch(function(err) { 
-        console.log("oops1"); 
+$(() => {
+    
+    navigator
+        .mediaDevices
+        .getUserMedia({ video: true, audio: false },)
+        .then((stream) => {
+            var mirror = document.querySelector('#video');
+            mirror.srcObject = stream;
+            mirror.play();        
+        })
+        .catch(function(err) { 
+            console.log("oops1"); 
+        });
+
+    $('#mirror').change(function () {
+        if ($(this).prop('checked')) {
+            $('#mirror-container').addClass('mirror');
+        } else {
+            $('#mirror-container').removeClass('mirror');
+        }
     });
 
-    $(function() {
-        $('#mirror').change(function() {
-            if ($(this).prop('checked')) {
-                $('#mirror-container').addClass('mirror')
-            } else {
-                $('#mirror-container').removeClass('mirror')
-            }
-        })
+    $('#sepia').change(function () {
+        if ($(this).prop('checked')) {
+            $('#sepia-container').addClass('sepia');
+        } else {
+            $('#sepia-container').removeClass('sepia');
+        }
+    });
 
-        $('#sepia').change(function() {
-            if ($(this).prop('checked')) {
-                $('#sepia-container').addClass('sepia')
-            } else {
-                $('#sepia-container').removeClass('sepia')
-            }
-        })
+    $('#night').change(function() {
+        if ($(this).prop('checked')) {
+            $('#brightness-container').addClass('bright');
+        } else {
+            $('#brightness-container').removeClass('bright');
+        }
+    });
 
-        $('#night').change(function() {
-            if ($(this).prop('checked')) {
-                $('#brightness-container').addClass('bright')
-            } else {
-                $('#brightness-container').removeClass('bright')
-            }
-        })
-
-        $('#blur').change(function() {
-            if ($(this).prop('checked')) {
-                $('#blur-container').addClass('blur')
-            } else {
-                $('#blur-container').removeClass('blur')
-            }
-        })
-      });
+    $('#blur').change(function () {
+        if ($(this).prop('checked')) {
+            $('#blur-container').addClass('blur');
+        } else {
+            $('#blur-container').removeClass('blur');
+        }
+    });
+});
